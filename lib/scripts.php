d63de92bb5a2f295aa15043cf197c4064d60ed7b
Erik James Albaugh <erik@maintainweb.co>
2014-05-19 16:49:04 -0700

    Icon Style Updates
    Resize on hover

--- lib/scripts.php
+++ lib/scripts.php
@@ -11,7 +11,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, 'ae7edd8f6833ac7dc2bb9b50ee16b84b');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, 'f955bb316d9005075419d2f4371a484e');
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
   // Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
